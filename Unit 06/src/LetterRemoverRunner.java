5bd04c9d88b4b69b4b2789e075249cea4c5a4612
Warren Chang <33472818+Bluebox303@users.noreply.github.com>
2019-02-12 01:01:58 -0800

    LetterRemover Done

--- Unit 06/src/LetterRemoverRunner.java
+++ Unit 06/src/LetterRemoverRunner.java
@@ -8,7 +8,21 @@ public class LetterRemoverRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases		
+		//add test cases
+		LetterRemover test = new LetterRemover("I am Sam I am", 'a');
+		out.println(test);
+		
+		test.setRemover("ssssssssssssssssssssss",'s');
+		out.print(test);
+		
+		test.setRemover("qwertyqwertyqwerty", 'a');
+		out.println(test);
+		
+		test.setRemover("ababababababab", 'b');
+		out.println(test);
+		
+		test.setRemover("ababababababab", 'x');
+		out.print(test);
 											
 	}
 }
