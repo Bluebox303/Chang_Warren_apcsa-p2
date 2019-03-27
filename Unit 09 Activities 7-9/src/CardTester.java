b5cf66780f057323be843e6a039e0abab1a35385
Warren Chang <33472818+Bluebox303@users.noreply.github.com>
2019-03-26 20:38:58 -0700

    Update

---
+++ Unit 09 Activities 7-9/src/CardTester.java
@@ -0,0 +1,23 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) 
+	{
+		Card cardOne = new Card("three", "spades", 2);
+		System.out.println(cardOne);
+		
+		Card cardTwo = new Card("five", "hearts", 6);
+		System.out.println(cardTwo);
+		
+		Card cardThree = new Card("jack", "spades", 17);
+		System.out.println(cardThree);
+		
+		System.out.println(cardThree.matches(cardOne));
+	}
+}
