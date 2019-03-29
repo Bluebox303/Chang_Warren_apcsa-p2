1a9d2d3cb1bc33b15787f83bdb0854c71bd285a5
Warren Chang <33472818+Bluebox303@users.noreply.github.com>
2019-03-29 10:29:17 -0700

    Update

--- Unit 12/src/Monster.java
+++ Unit 12/src/Monster.java
@@ -7,8 +7,13 @@ import static java.lang.System.*;
 public interface Monster
 {
 	public int getHowBig();
+	
 	public String getName();
+	
 	public boolean isBigger(Monster other);	
+	
 	public boolean isSmaller(Monster other);
+	
 	public boolean namesTheSame(Monster other);
+	
 }
