2a7dd8a690f1e8f03f4643df00c30b3055d845e9
Warren Chang <33472818+Bluebox303@users.noreply.github.com>
2019-05-06 10:12:51 -0700

    update

---
+++ Pong Extension/src/Collidable.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public interface Collidable
+{
+	boolean didCollideTop(Object obj);  
+	boolean didCollideBottom(Object obj);  
+	boolean didCollideLeft(Object obj);  
+	boolean didCollideRight(Object obj);  
+}
